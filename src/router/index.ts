a7cd6b3b9b0bd89b2ec0f3bd8f57ef78896f908b
CHEATEY13 <dixlina2@gmail.com>
2025-10-18 10:45:18 +0700

    Initial commit - Vue project

---
+++ src/router/index.ts
@@ -0,0 +1,23 @@
+import { createRouter, createWebHistory } from 'vue-router'
+// import HomeView from '../views/HomeView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: {template: '<div>This is my first view project</div>' }
+    },
+    {
+      path: '/about',
+      name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: {template: '<div>About Page</div>' }
+    },
+  ],
+})
+
+export default router
